1ce9cb23c273f0968d82d637b721a8b500146169
Martin Honermeyer <maze@strahlungsfrei.de>
2015-05-26 01:46:16 +0200

    Fix management container to get all needed ENV variables

--- management/start.sh
+++ management/start.sh
@@ -5,4 +5,7 @@ cat >> /etc/dovecot/conf.d/99-remote-doveadm.conf << EOF;
 doveadm_password = $DOVEADM_PASSWORD
 EOF
 
+# FIXME Put only needed in variables in mailinabox.conf. Even better, get rid of that extra file!
+env >/etc/mailinabox.conf
+
 exec /usr/local/bin/mailinabox-daemon
